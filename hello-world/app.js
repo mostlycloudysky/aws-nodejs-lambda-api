90764da70b98c901bd7258288753aefa950f0ec4
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2021-10-10 11:21:17 -0400

    in progress api development

--- hello-world/app.js
+++ hello-world/app.js
@@ -1,11 +1,30 @@
 const serverless = require('serverless-http');
 const express = require('express');
-
+const s3 = require('aws-sdk/clients/s3');
+const env = require('env-var');
+const bodyParser = require('body-parser');
 // @ts-ignore
 const app = new express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
+const plainTextParser = bodyParser.text();
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).json({ message: 'Internal Server Error'});
+})
+
+function newS3Client() {
+    return new s3({ params: { Bucket: env.get('BUCKET').required().asString() } })
+}
+
+function getAuthor() {
+    return 'anonymous';
+}
+
+app.get('/', async (req, res) => {
+    const client = newS3Client();
+    const maxItems = req.query.maxItems || 20;
+    const token = req.query.token;
 })
 
 module.exports.lambdaHandler = serverless(app);
