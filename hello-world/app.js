d7d94dfc3050a452c043fdaa37eb890a5dc03392
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2021-10-12 07:38:45 -0400

    Revert "in progress api development"
    This reverts commit 90764da70b98c901bd7258288753aefa950f0ec4.

--- hello-world/app.js
+++ hello-world/app.js
@@ -1,30 +1,11 @@
 const serverless = require('serverless-http');
 const express = require('express');
-const s3 = require('aws-sdk/clients/s3');
-const env = require('env-var');
-const bodyParser = require('body-parser');
+
 // @ts-ignore
 const app = new express();
 
-const plainTextParser = bodyParser.text();
-
-app.use((err, req, res, next) => {
-    console.log(err);
-    res.status(500).json({ message: 'Internal Server Error'});
-})
-
-function newS3Client() {
-    return new s3({ params: { Bucket: env.get('BUCKET').required().asString() } })
-}
-
-function getAuthor() {
-    return 'anonymous';
-}
-
-app.get('/', async (req, res) => {
-    const client = newS3Client();
-    const maxItems = req.query.maxItems || 20;
-    const token = req.query.token;
+app.get('/', (req, res) => {
+    res.send('Hello World')
 })
 
 module.exports.lambdaHandler = serverless(app);
